24abd5dedbb677d6ccd5219a049098e14e97e1df
Valerie K <valerik210695@gmail.com>
2025-05-30 12:06:00 +0300

    mobile header menu

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-const headerMenuButtons = document.querySelectorAll('.header__nav-button');
+const headerMenuList = document.querySelector('.header__main-list');
 const headerModal = document.querySelector('.header-modal');
 
-const openHeaderMenu = (evt) => {
-  evt.stopPropagation();
-  headerModal.classList.add('header-modal_opened');
+const clickHeaderMenu = (evt) => {
+  if (window.innerWidth < 1024) return;
+  if (!evt.target.classList.contains('header__nav-button') && !evt.target.closest('.header__nav-button')) return;
 
-  if (evt.target.classList.contains('header__nav-button') && !evt.target.classList.contains('header__nav-button_opened')) {
-    headerMenuButtons.forEach(item => {
-      item.classList.remove('header__nav-button_opened');
-    })
+  if (evt.target.classList.contains('header__nav-button_opened') || evt.target.closest('.header__nav-button_opened')) {
 
-    evt.target.classList.add('header__nav-button_opened');
-  }
+    if (evt.target.classList.contains('header__nav-button')) {
+      evt.target.classList.remove('header__nav-button_opened');
+    }
+  
+    if (evt.target.closest('.header__nav-button')) {
+      evt.target.closest('.header__nav-button').classList.remove('header__nav-button_opened');
+    }
 
-  if (evt.target.closest('.header__nav-button') && !evt.target.closest('.header__nav-button').classList.contains('header__nav-button_opened')) {
-    headerMenuButtons.forEach(item => {
-      item.classList.remove('header__nav-button_opened');
-    })
+    headerModal.classList.remove('header-modal_opened');
+    return;
+  } else {
+    const openedMenu = document.querySelector('.header__nav-button_opened');
 
-    evt.target.closest('.header__nav-button').classList.add('header__nav-button_opened');
+    if (openedMenu) {
+      openedMenu.classList.remove('header__nav-button_opened');
+      headerModal.classList.remove('header-modal_opened');
+    }
+
+    if (evt.target.classList.contains('header__nav-button')) {
+      evt.target.classList.add('header__nav-button_opened');
+    }
+  
+    if (evt.target.closest('.header__nav-button')) {
+      evt.target.closest('.header__nav-button').classList.add('header__nav-button_opened');
+    }
+
+    headerModal.classList.add('header-modal_opened');
   }
 }
 
-const closeHeaderMenu = (evt) => {
-  evt.stopPropagation();
-
-  headerMenuButtons.forEach(item => {
-    item.classList.remove('header__nav-button_opened');
-  })
-
-  if (evt.target.classList.contains('header__nav-button')) {
-    evt.target.classList.add('header__nav-button_opened');
-    return;
-  }
-
+const closeHeaderMenuOverlay = (evt) => {
+  if (window.innerWidth < 1024) return;
+  const openedMenu = document.querySelector('.header__nav-button_opened');
+  if (!openedMenu || evt.target.classList.contains('header__nav-button') || evt.target.closest('.header__nav-button')) return;
+  openedMenu.classList.remove('header__nav-button_opened');
   headerModal.classList.remove('header-modal_opened');
 }
 
-document.addEventListener('click', closeHeaderMenu);
-
-headerMenuButtons.forEach(item => {
-  item.addEventListener('click', openHeaderMenu);
-})
+headerMenuList.addEventListener('click', clickHeaderMenu);
+document.addEventListener('click', closeHeaderMenuOverlay);
